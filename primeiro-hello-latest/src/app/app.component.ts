0a9b5430d027712e0a88ad6643ede8fba19883ce
Guilherme Martins <guimsl2004@gmail.com>
2025-01-14 16:51:31 -0300

    criando novo component ng g component (nome do componente, ou criar ele numa pasta e nome)

--- primeiro-hello-latest/src/app/app.component.ts
+++ primeiro-hello-latest/src/app/app.component.ts
@@ -1,15 +1,17 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { NewComponent } from './components/new-component/new-component.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule ,RouterOutlet],
+  imports: [CommonModule ,RouterOutlet, NewComponent],
   template: `
     <router-outlet/> 
     <h1>Hello</h1>
-  
+    <app-new-component />
+    
   `,
 })
 export class AppComponent {
