87f247811d65e2f356e20f304afb3e63ee4a9ac9
Guilherme Martins <guimsl2004@gmail.com>
2025-01-14 16:38:17 -0300

    primeir hello literalement

--- primeiro-hello-latest/src/app/app.component.ts
+++ primeiro-hello-latest/src/app/app.component.ts
@@ -1,11 +1,16 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  standalone: true,
+  imports: [CommonModule ,RouterOutlet],
+  template: `
+    <router-outlet/> 
+    <h1>Hello</h1>
+  
+  `,
 })
 export class AppComponent {
   title = 'primeiro-hello-latest';
