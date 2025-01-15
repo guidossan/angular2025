1d74ea9ee53b868f59e7743453d7f73ff0744254
Guilherme Martins <guimsl2004@gmail.com>
2025-01-15 11:25:29 -0300

    ngModel

--- primeiro-hello-latest/src/app/components/template/template-binding/template-binding.component.ts
+++ primeiro-hello-latest/src/app/components/template/template-binding/template-binding.component.ts
@@ -1,9 +1,10 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-template-binding',
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule],
   templateUrl: './template-binding.component.html',
   styleUrl: './template-binding.component.scss'
 })
