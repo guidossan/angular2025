42c37ac83543c35513c6410926a5995c221f5b0a
Guilherme Martins <guimsl2004@gmail.com>
2025-01-15 14:06:44 -0300

    Forma de controle de fluxo no html, if else

--- primeiro-hello-latest/src/app/app.component.ts
+++ primeiro-hello-latest/src/app/app.component.ts
@@ -4,11 +4,12 @@ import { RouterOutlet } from '@angular/router';
 import { NewComponent } from './components/new-component/new-component.component';
 import { TemplateBindingComponent } from './components/template/template-binding/template-binding.component';
 import { TemplateVariablesComponent } from "./components/template/template-variables/template-variables.component";
+import { TemplateControlFlowComponent } from "./components/template/template-control-flow/template-control-flow.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, NewComponent, TemplateBindingComponent, TemplateVariablesComponent],
+  imports: [CommonModule, RouterOutlet, NewComponent, TemplateBindingComponent, TemplateVariablesComponent, TemplateControlFlowComponent],
   template: `
     <router-outlet/>
     <h1>Hello</h1>
@@ -16,7 +17,8 @@ import { TemplateVariablesComponent } from "./components/template/template-varia
 
       <!--<app-new-component />-->
       <!--<app-template-binding/>-->
-      <app-template-variables/>
+      <!--<app-template-variables/>-->
+      <app-template-control-flow/>
     </div>
 
   `,
