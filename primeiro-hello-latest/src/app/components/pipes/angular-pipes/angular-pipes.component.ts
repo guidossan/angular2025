3af7dceb77c9af28f0d77b0852e1b369efab6e9b
Guilherme Martins <guimsl2004@gmail.com>
2025-01-28 14:44:43 -0300

    localidade para lidar com numeros, porcentagem

--- primeiro-hello-latest/src/app/components/pipes/angular-pipes/angular-pipes.component.ts
+++ primeiro-hello-latest/src/app/components/pipes/angular-pipes/angular-pipes.component.ts
@@ -1,13 +1,25 @@
-import { AsyncPipe, CommonModule, DatePipe, LowerCasePipe, UpperCasePipe } from '@angular/common';
-import { Component, signal } from '@angular/core';
+import { AsyncPipe, CommonModule, CurrencyPipe, DatePipe, DecimalPipe, LowerCasePipe, PercentPipe, registerLocaleData, UpperCasePipe } from '@angular/common';
+import { Component, LOCALE_ID, signal } from '@angular/core';
 import { delay, Observable, of } from 'rxjs';
+import LocalePt from '@angular/common/locales/pt';
+registerLocaleData(LocalePt);
 
 @Component({
   selector: 'app-angular-pipes',
   standalone: true,
-  imports: [CommonModule, DatePipe, UpperCasePipe, LowerCasePipe, AsyncPipe],
+  imports: [
+    CommonModule,
+    DatePipe,
+    UpperCasePipe,
+    LowerCasePipe,
+    AsyncPipe,
+    CurrencyPipe,
+    DecimalPipe,
+    PercentPipe
+  ],
   templateUrl: './angular-pipes.component.html',
-  styleUrl: './angular-pipes.component.scss'
+  styleUrl: './angular-pipes.component.scss',
+  providers: [{ provide: LOCALE_ID, useValue: 'pt-BR' }]
 })
 export class AngularPipesComponent {
   public date = signal(new Date());
@@ -18,4 +30,6 @@ export class AngularPipesComponent {
     'Please wait...',
     'Fetching data...'
   ]).pipe(delay(2000));
+
+
 }
