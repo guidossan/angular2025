caef152a3d12628da9fabb7f1149add45ff50f82
Guilherme Martins <guimsl2004@gmail.com>
2025-02-06 13:44:44 -0300

    sempre que criar um componente novo ja vem com changedetection e standalone true, ts config caminho relativo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,18 @@
 import { ChangeDetectionStrategy, Component, OnInit, signal } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
-import { NewComponent } from './components/new-component/new-component.component';
-import { TemplateBindingComponent } from './components/template/template-binding/template-binding.component';
-import { TemplateVariablesComponent } from "./components/template/template-variables/template-variables.component";
-import { TemplateControlFlowComponent } from "./components/template/template-control-flow/template-control-flow.component";
-import { TemplateDeferrableViewsComponent } from './components/template/template-deferrable-views/template-deferrable-views.component';
-import { SignalsComponent } from './components/signals/signals.component';
-import { AngularPipesComponent } from "./components/pipes/angular-pipes/angular-pipes.component";
-import { ReactiveFormsComponent } from './components/forms/reactive-forms/reactive-forms.component';
-import { TemplateDrivenFormsComponent } from "./components/forms/template-driven-forms/template-driven-forms.component";
-import { ContentComponent } from "./components/content/content.component";
-import { HostElementsComponent } from "./components/host-elements/host-elements.component";
-import { LifeCycleComponentComponent } from "./components/life-cycle-component/life-cycle-component.component";
+import { NewComponent } from '@components/new-component/new-component.component';
+import { TemplateBindingComponent } from '@components/template/template-binding/template-binding.component';
+import { TemplateVariablesComponent } from "@components/template/template-variables/template-variables.component";
+import { TemplateControlFlowComponent } from "@components/template/template-control-flow/template-control-flow.component";
+import { TemplateDeferrableViewsComponent } from '@components/template/template-deferrable-views/template-deferrable-views.component';
+import { SignalsComponent } from '@components/signals/signals.component';
+import { AngularPipesComponent } from "@components/pipes/angular-pipes/angular-pipes.component";
+import { ReactiveFormsComponent } from '@components/forms/reactive-forms/reactive-forms.component';
+import { TemplateDrivenFormsComponent } from "@components/forms/template-driven-forms/template-driven-forms.component";
+import { ContentComponent } from "@components/content/content.component";
+import { HostElementsComponent } from "@components/host-elements/host-elements.component";
+import { LifeCycleComponentComponent } from "@components/life-cycle-component/life-cycle-component.component";
 
 
 @Component({
